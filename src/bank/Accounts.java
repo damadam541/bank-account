22d2f024fcfcf87eac3d2e9081879bc0d45c4fe6
damadam541 <damadam541@gmail.com>
2022-05-25 10:39:37 -0400

    Added Variables to Accounts.java

--- src/bank/Accounts.java
+++ src/bank/Accounts.java
@@ -1,5 +1,15 @@
 package bank;
 
 public class Accounts {
+	
+	int bankNum;
+	String name;
+	double total;
 
+	Accounts(int bankNum, String name, double total){
+		this.bankNum = bankNum;
+		this.name = name;
+		this.total = total;
+	}
+	
 }
--- src/bank/BankAccount.java
+++ src/bank/BankAccount.java
@@ -1,10 +1,11 @@
 package bank;
 
+import java.util.ArrayList;
+
 public class BankAccount {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
